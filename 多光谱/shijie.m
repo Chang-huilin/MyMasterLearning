408bfa1ea8a468167cd8ed28b123d2ac3b15252b
ChangHL <changhuilin98@gmail.com>
2023-10-24 19:20:05 +0800

    1

--- 多光谱/shijie.m
+++ 多光谱/shijie.m
@@ -13,26 +13,26 @@ plspvsm(predModel,num_total,1,1);      %预测集的结果
 
 
 
-% % CARS
-% MCCV=plsmccv(X,Y,15,'center',1000,0.8);
-% CARS=carspls(X,Y,MCCV.optPC,5,'center',50); 
-% plotcars(CARS);
-% SelectedVariables=CARS.vsel;
+% CARS
+MCCV=plsmccv(X,Y,15,'center',1000,0.8);
+CARS=carspls(X,Y,MCCV.optPC,5,'center',50); 
+plotcars(CARS);
+SelectedVariables=CARS.vsel;
 
-% Xt3=Xt(:,SelectedVariables);
-% Xc3=Xc(:,SelectedVariables);
+Xt3=Xt(:,SelectedVariables);
+Xc3=Xc(:,SelectedVariables);
 
-% Model=ipls(Xc3,Yc,10,'mean',1,[],'syst123',5);
-% iplsplot(Model,'intlabel')
-% plsrmse(Model,0)
+Model=ipls(Xc3,Yc,10,'mean',1,[],'syst123',5);
+iplsplot(Model,'intlabel')
+plsrmse(Model,0)
 
-% num_total =8;   %主成分数
-% plspvsm(Model,num_total,1);        %更改主成分数
-% oneModel=plsmodel(Model,1,num_total,'mean','syst123',5);
-% predModel=plspredict(Xc3,oneModel,num_total,Yc);
-% plspvsm(predModel,num_total)
-% predModel=plspredict(Xt3,oneModel,num_total,Yt);
-% plspvsm(predModel,num_total)
+num_total =8;   %主成分数
+plspvsm(Model,num_total,1);        %更改主成分数
+oneModel=plsmodel(Model,1,num_total,'mean','syst123',5);
+predModel=plspredict(Xc3,oneModel,num_total,Yc);
+plspvsm(predModel,num_total)
+predModel=plspredict(Xt3,oneModel,num_total,Yt);
+plspvsm(predModel,num_total)
 
 % Variables = xaxis(SelectedVariables,1);  %SelectedVariables选出来的变量从xaxis里摘出来,参数不变    
 
